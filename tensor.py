8007f5b2c2f5d2aed0400616c1a97667fff2a3e2
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-03-07 01:37:52 +0100

    update

--- tensor.py
+++ tensor.py
@@ -1,21 +1,56 @@
-# import backend as be
-import backend_einsum as be
-from abc import abstractmethod, ABC
-from typing import Final, Callable
-from string import ascii_letters
-from copy import copy
+# =========================================================
+# Backend specific functions
+# =========================================================
+import cupy as cp
+import symbolic
+import math
+
+Data_t = cp.ndarray
+
+
+def array(data) -> Data_t:
+    return cp.array(data, dtype=float)
+
+
+def ein_eval(expr: str, *Ts: Data_t, bounds: dict[str, int], threads_per_block=128) -> Data_t:
+    program = symbolic.prog_cuda(expr, bounds, [T.shape for T in Ts])
+    # print(program)
+    program_gpu = cp.RawKernel(program, "ein")
+    out_shape = symbolic.get_output_shape(expr, bounds)
+    size = math.prod(out_shape)
+    out_gpu = cp.zeros(size, dtype=float)
+
+    grid_size = (int(math.ceil(size / threads_per_block)), 1, 1)
+    block_size = (threads_per_block, 1, 1)
+
+    program_gpu(grid_size, block_size, (*[T.flatten("F") for T in Ts], out_gpu))
+    return out_gpu.reshape(out_shape, order="F")
+
+
+def delta(shape: tuple[int]) -> Data_t:
+    args_a = symbolic.make_indices(len(shape))
+    args_b = symbolic.make_indices(len(shape), set(args_a))
+    e = f"T({','.join(args_a)},{','.join(args_b)})<-{symbolic.delta_prod(args_a, args_b)}"
+    return ein_eval(e, bounds=dict(zip(args_a + args_b, shape + shape)))
+
+
+# =========================================================
+# Backend agnostic classes/functions
+# =========================================================
+from typing import Callable
+from abc import ABC, abstractmethod
 
 
 class Expression(ABC):
-    data: be.Data_t
-    shape: Final[tuple[int]]
+    data: Data_t
+    shape: tuple[int]
 
     @abstractmethod
-    def eval(self) -> be.Data_t:
+    def eval(self) -> Data_t:
         pass
 
     @abstractmethod
-    def grad(self, seed: be.Data_t) -> None:
+    def grad(self, seed: Data_t) -> None:
         pass
 
     @abstractmethod
@@ -28,11 +63,13 @@ class Expression(ABC):
         return Add(self, other)
 
     def __mul__(self, other):
-        if not isinstance(other, float):
+        if not isinstance(other, (float, int)):
             raise TypeError("Unsupported operand type(s) for *")
         return Mul(self, other)
 
     def __rmul__(self, other):
+        if not isinstance(other, (float, int)):
+            raise TypeError("Unsupported operand type(s) for *")
         return Mul(self, other)
 
     def __sub__(self, other):
@@ -41,206 +78,203 @@ class Expression(ABC):
         return self + -1 * other
 
     def __pow__(self, other):
-        if not isinstance(other, float):
+        if not isinstance(other, (float, int)):
             raise TypeError("Unsupported operand type(s) for **")
         return Pow(self, other)
 
 
 class Tensor(Expression):
-    def __init__(self, data: be.Data_t):
-        self.data: be.Data_t = data
-        self.shape = data.shape
-        self.partial: be.Data_t = None
+    def __init__(self, data) -> None:
+        self.data: Data_t = array(data)
+        self.shape: tuple[int] = self.data.shape
+        self.partial: Data_t = None
 
-    def eval(self):
-        # print("leaf")
+    def eval(self) -> Data_t:
         return self.data
 
-    def grad(self, seed: be.Data_t = None):
-        seed = seed if seed is not None else be.delta(self.shape)
-        self.partial = self.partial + seed if self.partial is not None else seed
+    def grad(self, seed: Data_t = None) -> None:
+        seed = delta(self.shape) if seed is None else seed
+        self.partial = seed if self.partial is None else self.partial + seed
 
-    def null(self):
+    def null(self) -> None:
         self.partial = None
 
 
-# -----------------------------------
-# Binary Ops
-# -----------------------------------
-
-
+# ---------------------------------------------------------
+# Tensor addition
+# ---------------------------------------------------------
 class Add(Expression):
-    def __init__(self, A: Expression, B: Expression):
-        self.A = A
-        self.B = B
+    def __init__(self, T1: Expression, T2: Expression):
+        assert T1.shape == T2.shape
+        self.T1, self.T2 = T1, T2
+        self.data: Data_t = None
+        self.shape = T1.shape
 
-        assert A.shape == B.shape, "You can only add tensors of the same shape"
-        self.data: be.Data_t = None
-        self.shape = A.shape
-
-    def eval(self):
-        self.data = self.A.eval() + self.B.eval() if self.data is None else self.data
+    def eval(self) -> Data_t:
+        self.data = self.T1.eval() + self.T2.eval() if self.data is None else self.data
         return self.data
 
-    def grad(self, seed: be.Data_t = None):
-        seed = seed if seed is not None else be.delta(self.shape)
-        self.A.grad(seed)
-        self.B.grad(seed)
+    def grad(self, seed: Data_t = None) -> None:
+        seed = delta(self.shape) if seed is None else seed
+        self.T1.grad(seed)
+        self.T2.grad(seed)
+
+    def null(self) -> None:
+        self.data = None
+        self.T1.null()
+        self.T2.null()
+
+
+# ---------------------------------------------------------
+# Multiplication by a scalar
+# ---------------------------------------------------------
+class Mul(Expression):
+    def __init__(self, T: Expression, a: float | int):
+        self.T = T
+        self.a = a
+        self.data: Data_t = None
+        self.shape = T.shape
+
+    def eval(self):
+        self.data = self.a * self.T.eval() if self.data is None else self.data
+        return self.data
+
+    def grad(self, seed: Data_t = None):
+        seed = delta(self.shape) if seed is None else seed
+        self.T.grad(self.a * seed)
 
     def null(self):
         self.data = None
-        self.A.null()
-        self.B.null()
+        self.T.null()
 
 
-class Einsum(Expression):
-    def __init__(self, indices: str, A: Expression, B: Expression):
-        self.A = A
-        self.B = B
-        self.indices: str = indices
+# ---------------------------------------------------------
+# Einstein's summation notation
+# ---------------------------------------------------------
+class Ein(Expression):
+    def __init__(self, expr: str, *Ts: Expression, bounds: dict[str, int]) -> None:
+        self.Ts: tuple[Expression] = Ts
+        self.expr: str = expr
+        self.data: Data_t = None
+        self.shape: tuple[int] = symbolic.get_output_shape(expr, bounds)
+        self.bounds: dict[str] = bounds
 
-        self.data: be.Data_t = None
-        self.shape = get_shape(indices, A.shape, B.shape)
-
-    def eval(self):
+    def eval(self) -> Data_t:
         self.data = (
-            be.einsum(self.indices, self.A.eval(), self.B.eval())
+            ein_eval(self.expr, *[T.eval() for T in self.Ts], bounds=self.bounds)
             if self.data is None
             else self.data
         )
         return self.data
 
-    # TODO: It can be made faster with delta reduction
-    def grad(self, seed: be.Data_t = None):
-        seed = seed if seed is not None else be.delta(self.shape)
-        alphabet = set(ascii_letters) - set(self.indices)
-        ij, k = self.indices.split("->")
+    def grad(self, seed: Data_t = None) -> None:
+        seed = delta(self.shape) if seed is None else seed
+        Tid = 0
+        for T, (grad_args, free_args, grad_expr) in zip(self.Ts, symbolic.derive(self.expr)):
+            _, rhs = grad_expr.split("<-")
+            new_free_args = symbolic.make_indices(
+                len(seed.shape) - len(free_args),
+                set(grad_args) | set(free_args) | set(self.bounds.keys()),
+            )
+            expr = f"T({','.join(grad_args.split(','))},{','.join(new_free_args)}) <- T({','.join(free_args)},{','.join(new_free_args)}){'*'+rhs if len(rhs)>0 else ''}"
+            bounds = {
+                **self.bounds,
+                **dict(zip(grad_args.split(","), T.shape)),
+                **dict(zip(new_free_args, seed.shape[-len(new_free_args) :])),
+            }
+            print(expr)
+            new_seed = ein_eval(
+                expr,
+                seed,
+                *([T.eval() for T in self.Ts[:Tid]] + [T.eval() for T in self.Ts[Tid + 1 :]]),
+                bounds=bounds,
+            )
+            T.grad(new_seed)
+            Tid += 1
 
-        i, j = ij.split(",")
-        a, b = make_indices(alphabet, len(seed.shape) - len(self.shape), len(self.A.shape))
-        print(f"{k+a},{b+i},{j}->{b+a}")
-
-        j_ = j.translate(str.maketrans({k: v for k, v in zip(i, b)}))
-        k_ = k.translate(str.maketrans({k: v for k, v in zip(i, b)}))
-        equal = ["".join(nb for nb, ni in zip(b, i) if ni == n) for n in set(i)]
-        print(f"{k_+a},{equal},{j_}->{b+a}")
-
-        new_seed = be.einsum(f"{k+a},{b+i},{j}->{b+a}", seed, be.Delta(self.A.shape), self.B.eval())
-        self.A.grad(new_seed)
-
-        i, j = ij.split(",")
-        a, b = make_indices(alphabet, len(seed.shape) - len(self.shape), len(self.B.shape))
-        print(f"{k+a},{i},{b+j}->{b+a}")
-        new_seed = be.einsum(f"{k+a},{i},{b+j}->{b+a}", seed, self.A.eval(), be.Delta(self.B.shape))
-        self.B.grad(new_seed)
-
-    def null(self):
+    def null(self) -> None:
         self.data = None
-        self.A.null()
-        self.B.null()
+        for T in self.Ts:
+            T.null()
 
 
-# -----------------------------------
-# Unary Ops
-# -----------------------------------
+# ---------------------------------------------------------
+# Element-wise function
+# ---------------------------------------------------------
+class Elf(Expression):
+    f: Callable[[Data_t], Data_t]
+    df: Callable[[Data_t], Data_t]
 
+    def __init__(self, T: Expression):
+        self.T = T
+        self.data: Data_t = None
+        self.shape = T.shape
 
-class Mul(Expression):
-    def __init__(self, A: Expression, a: float):
-        self.A = A
-        self.a = a
-
-        self.data: be.Data_t = None
-        self.shape = A.shape
-
-    def eval(self):
-        self.data = self.a * self.A.eval() if self.data is None else self.data
+    def eval(self) -> Data_t:
+        self.data = self.f(self.T.eval()) if self.data is None else self.data
         return self.data
 
-    def grad(self, seed: be.Data_t = None):
-        seed = seed if seed is not None else be.delta(self.shape)
-        self.A.grad(self.a * seed)
+    def grad(self, seed: Data_t = None) -> None:
+        seed = delta(self.shape) if seed is None else seed
+        args_a = ",".join(symbolic.make_indices(len(seed.shape) - len(self.shape)))
+        args_b = ",".join(symbolic.make_indices(len(self.shape), set(args_a)))
+
+        expr = f"T({args_b},{args_a}) <- T({args_b},{args_a}) * T({args_b})"
+        bounds = {
+            **dict(zip(args_b.split(","), self.shape)),
+            **dict(zip(args_a.split(","), seed.shape[-len(args_a.split(",")) :])),
+        }
+        new_seed = ein_eval(expr, seed, self.df(self.T.eval()), bounds=bounds)
+        self.T.grad(new_seed)
 
     def null(self):
         self.data = None
-        self.A.null()
+        self.T.null()
 
 
-class Function1D(Expression):
-    f: Callable[[float], float]
-    df: Callable[[float], float]
-
-    def __init__(self, A: Expression):
-        self.A = A
-        self.data: be.Data_t = None
-        self.shape = A.shape
-
-    def eval(self):
-        self.data = self.f(self.A.eval()) if self.data is None else self.data
-        return self.data
-
-    def grad(self, seed: be.Data_t = None):
-        seed = seed if seed is not None else be.delta(self.shape)
-        a, b = make_indices(set(ascii_letters), len(seed.shape) - len(self.shape), len(self.shape))
-        new_seed = be.einsum(f"{b+a},{b}->{b+a}", seed, self.df(self.A.eval()))
-        self.A.grad(new_seed)
-
-    def null(self):
-        self.data = None
-        self.A.null()
-
-
-class Pow(Function1D):
-    def __init__(self, A: Expression, e: float):
+class Pow(Elf):
+    def __init__(self, T: Expression, e: float | int):
         self.f = lambda x: x**e
         self.df = lambda x: e * x ** (e - 1)
-        super().__init__(A)
+        super().__init__(T)
 
 
-class ReLU(Function1D):
-    def __init__(self, A: Expression):
-        self.f = lambda x: be.max(0, x)
-        self.df = lambda x: be.heaviside(x, 0)
-        super().__init__(A)
+class Exp(Elf):
+    def __init__(self, T: Expression):
+        self.f = lambda x: cp.exp(x)
+        self.df = lambda x: cp.exp(x)
+        super().__init__(T)
 
 
-# -----------------------------------
-# Reshape Ops
-# -----------------------------------
+class Log(Elf):
+    def __init__(self, T: Expression):
+        self.f = lambda x: cp.log(x)
+        self.df = lambda x: 1 / x
+        super().__init__(T)
 
 
-class Reshape(Expression):
-    pass
+class ReLU(Elf):
+    def __init__(self, T: Expression):
+        self.f = lambda x: cp.maximum(0, x)
+        self.df = lambda x: cp.heaviside(x, 0)
+        super().__init__(T)
 
 
-# -----------------------------------
-# Utils
-# -----------------------------------
+import numpy as np
 
-
-# TODO: Make it nicer
-def get_shape(indices: str, sh1: tuple[int], sh2: tuple[int]):
-    xys, zs = indices.split("->")
-    xs, ys = xys.split(",")
-    shape = [None for _ in zs]
-    for i, z in enumerate(zs):
-        for j, x in enumerate(xs):
-            if x == z and shape[i] is None:
-                shape[i] = sh1[j]
-            elif x == z:
-                assert shape[i] == sh1[j], f"Not matching dimensions {j}"
-        for j, y in enumerate(ys):
-            if y == z and shape[i] is None:
-                shape[i] = sh2[j]
-            elif y == z:
-                assert shape[i] == sh2[j], f"Not matching dimensions {j}"
-
-    for s in shape:
-        assert s is not None, "One of the axis is None"
-
-    return tuple(shape)
-
-
-def make_indices(alphabet: set[str], *ns):
-    alphabet = copy(alphabet)
-    return ["".join(alphabet.pop() for _ in range(n)) for n in ns]
+if __name__ == "__main__":
+    # A = Tensor(np.ones((4000, 4000)))
+    # X = Tensor(np.ones(4000))
+    # C = Tensor([2])
+    # Y = Ein("T(i) <- T(i,j) * T(j) * T(0)", A, X, C, bounds={"i": 4000, "j": 4000})
+    # l = Ein("T(i) <- T(j)", Y, bounds={"i": 1, "j": 4000})
+    # print(l.eval())
+    # l.grad()
+    # # print(X.partial)
+    # print(A.partial[:, :, 0])
+    # print(C.partial)
+    X = Tensor([1])
+    Y = 0.5 * X**2
+    print(Y.eval())
+    Y.grad()
+    print(X.partial)
